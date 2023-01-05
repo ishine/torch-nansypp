b3a890981325d723de088127c5797effd45fe324
syj901220 <syj901220@naver.com>
2023-01-05 05:07:39 +0000

    chagne infer

--- inference.py
+++ inference.py
@@ -25,25 +25,25 @@ nansypp.to(device)
 nansypp.eval()
 
 # load wav
+# pack
 SR = nansypp.config.sr
 wav1, _ = librosa.load(args.wav1, sr=SR)
 wav2, _ = librosa.load(args.wav2, sr=SR)
 wavs = [wav1, wav2]
-# pack
 wavlen = np.array([len(w) for w in wavs])
-wav = np.stack([np.pad(w, [0, wavlen.max() - len(w)]) for w in wavs], axis=0)
 # convert
 wavlen = torch.tensor(wavlen, device=device)
-wav = torch.tensor(wav, device=device)
 
+wav1 = torch.tensor([wav1], device=device)
+wav2 = torch.tensor([wav2], device=device)
 with torch.no_grad():
     # [B, mel, T] reconstruction
-    synth = nansypp.forward(wav[0,][None], wav[0,][None], wavlen[0,][None], wavlen[0,][None])
+    synth = nansypp.forward(wav1, wav1, wavlen[0,][None], wavlen[0,][None])
     print(f'[*] reconstruct {synth.shape}')
     HOP = 256
     for i, (w, l) in enumerate(zip(synth, wavlen)):
         sf.write(os.path.join(args.out_dir, f'rctor{i}.wav'), w.cpu().numpy()[:l.item() * HOP], SR, 'PCM_24')
-    synth = nansypp.forward(wav[1,][None], wav[1,][None], wavlen[1,][None], wavlen[1,][None])
+    synth = nansypp.forward(wav2, wav2, wavlen[1,][None], wavlen[1,][None])
     print(f'[*] reconstruct {synth.shape}')
 
     HOP = 256
@@ -51,14 +51,14 @@ with torch.no_grad():
         sf.write(os.path.join(args.out_dir, f'rctor{1}.wav'), w.cpu().numpy()[:l.item() * HOP], SR, 'PCM_24')
         
     # [B, mel, T] reconstruction
-    synth = nansypp.forward(wav[0,][None], wav[1,][None], wavlen[0,][None], wavlen[1,][None])
+    synth = nansypp.forward(wav1, wav2, wavlen[0,][None], wavlen[1,][None])
     print(f'[*] vc {synth.shape}')
 
     HOP = 256
     for i, (w, l) in enumerate(zip(synth, wavlen)):
         sf.write(os.path.join(args.out_dir, f'vc{i}.wav'), w.cpu().numpy()[:l.item() * HOP], SR, 'PCM_24')
     # [B, mel, T] reconstruction
-    synth = nansypp.forward(wav[1,][None], wav[0,][None], wavlen[1,][None], wavlen[0,][None])
+    synth = nansypp.forward(wav2, wav1, wavlen[1,][None], wavlen[0,][None])
     print(f'[*] vc {synth.shape}')
 
     HOP = 256
